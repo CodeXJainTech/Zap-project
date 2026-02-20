6821146e619c79d0c2eed996ee431baa5baa9b0b
Anchal Jain <anchaljain1106@gmail.com>
2026-02-20 18:16:50 +0530

    metadata added to schema, server started

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,14 +1,38 @@
 import express from "express"
-
+import { PrismaClient } from "@prisma/client"; 
 const app = express();
+app.use(express.json());
+const client = new PrismaClient();
 
 //password logic
-app.post("/hooks/catch/:userId/:zapId", (req,res) =>{
+
+app.post("/hooks/catch/:userId/:zapId", async (req,res) =>{
   const userId = req.params.userId;
   const zapId = req.params.zapId;
-
+  const body = req.body;
+  console.log(body);
   //store in db a new trigger
-
+  await client?.$transaction(async tx => {
+    const run = await client.zapRun.create({
+      data:{
+        zapId: zapId,
+        metadata: body
+      }
+    });
+  
+    await client.zapRunOutbox.create({
+      data: {
+        zapRunId: run.id
+      }
+    })
+  });
+  res.json({
+    "message": "recieved."
+  })
   //push it on to a queue kafka or redis
+})
+
+app.listen(3000, () =>{
+  console.log("server started at 3000");
 })
 
