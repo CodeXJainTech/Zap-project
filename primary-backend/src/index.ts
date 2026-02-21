b3e21909c8538b96e8b87ed1de518232f6f59778
Anchal Jain <anchaljain1106@gmail.com>
2026-02-22 01:23:53 +0530

    primary-backend-v1 added

---
+++ primary-backend/src/index.ts
@@ -0,0 +1,16 @@
+import express from "express"
+import { userRouter } from "./router/user.js";
+import { zapRouter } from "./router/zap.js";
+import cors from "cors"
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("api/v1/user", userRouter);
+app.use("api/v1/zap", zapRouter);
+
+app.listen(3000, () => {
+  console.log("server started!")
+})
